8e722923d450b580a6a0e5ecb875aaa0e6cb23a0
libing <libing@healink.cn>
2023-05-31 07:12:45 +0800

    MOD: add log

--- tool_test.py
+++ tool_test.py
@@ -1,6 +1,7 @@
 
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
 from langchain.chat_models import ChatOpenAI
+from loguru import logger
 
 from app.tools.web_query_tool import WebpageQATool
 
@@ -10,4 +11,5 @@ query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
 if __name__ == "__main__":
     url = "https://zhuanlan.zhihu.com/p/64506277"
     question = "四岁女孩值的推荐的童书"
-    query_website_tool.run({"url": url, "question": question})
+    ret = query_website_tool.run({"url": url, "question": question})
+    logger.info("-> final result : {}", ret)
