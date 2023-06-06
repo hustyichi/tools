7e88b76078206e50b99ec41c42dbc3a154af5776
libing <libing@healink.cn>
2023-06-06 19:53:59 +0800

    ADD: add tool test

---
+++ tool_test.py
@@ -0,0 +1,13 @@
+
+from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
+from langchain.chat_models import ChatOpenAI
+
+from app.tools.web_query_tool import WebpageQATool
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1.0)
+query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
+
+if __name__ == "__main__":
+    url = "https://zhuanlan.zhihu.com/p/64506277"
+    question = "四岁女孩值的推荐的童书"
+    query_website_tool.run(url, question)
